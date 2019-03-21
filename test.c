ca3ab6450106a930d55dd4f7ae576ebb9a1bb8f7
Sean Mc Fadden <D00165887@student.dkit.ie>
2019-03-21 20:02:45 +0000

    "Bug Fixing"

--- test.c
+++ test.c
@@ -8,7 +8,8 @@ int main(){
 	//create a random bytes between 2048 and 20480
 	//Using Srand to change the seed so the same random number isnt used each run
 	srand(time(NULL));
-	int r = rand() % 5120 +512; //int = 4 bytes 20480/4 = 5120 2148/4=512 
+	int r = rand() % 20480 + 2018;
+	int datastart = rand() % 45055; //65535 -20480
 
 
 	//Allocate a block of memory, this will be 2^16 (Not 2^16-1, can assume full address space)
@@ -29,8 +30,14 @@ int main(){
 		return 1;
 	}
 
-	//printf("The random number generated was: %d\n", r);
-  //printf("address space: %d\n", addspace);
+	for(int i = 0; i < r; i++)
+	{
+		allocatemem[datastart] = 49;
+		datastart++;
+	}
+
+	printf("The random number generated was: %d and: %d\n", r, datastart);
+  printf("%s\n", allocatemem);
 	//printf("memory allocated: %zu\n ", sizeof(&allocatemem));
 	
 	free(allocatemem);
